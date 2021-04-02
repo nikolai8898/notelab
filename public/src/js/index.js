eb7f6ff90b217e299e6203851efedeeddb01cefb
nikolai8898 <afonkinnikolai@gmail.com>
2021-04-02 14:46:36 +0500

    summ

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -11,3 +11,29 @@ function reviewsSwipeRight() {
     swiper.style.marginLeft === "-100%" ? swiper.style.marginLeft = "0px" : {}
     swiper.style.marginLeft === "-200%" ? swiper.style.marginLeft = "-100%" : {}
 }
+
+
+
+
+
+
+
+// Новый код
+function formGO() {
+    const body = document.getElementById('body')
+    const modal = document.getElementById('modal')
+    const bodyGo = document.getElementById('go-back')
+    modal.style.display = "none"
+    body.style.display = "none"
+    bodyGo.style.display = "block"
+}
+function goBack() {
+    const body = document.getElementById('body')
+    const bodyGo = document.getElementById('go-back')
+    body.style.display = "block"
+    bodyGo.style.display = "none"
+}
+function showModal() {
+    const modal = document.getElementById('modal')
+    modal.style.display = "block"
+}
